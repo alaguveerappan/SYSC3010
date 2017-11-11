823b48b7500e230a29d081598985f5c3d1aec07c
Carmen Ho <carmenho@cmail.carleton.ca>
2017-11-10 22:51:40 -0500

    Add testArduino.py

---
+++ testArduino2.py
@@ -0,0 +1,25 @@
+import serial
+import time
+
+ser = serial.Serial("/dev/ttyACM1", 9600)
+pin = 1234
+count = 0
+
+while True:
+	read_ser = int(ser.readline())
+	print(read_ser)
+	
+	if read_ser == pin:
+		ser.write('1'.encode())
+		count = 0
+		
+	else: 
+		ser.write('0'.encode())
+		count += 1
+		
+		if(count >= 5):
+			ser.write('5'.encode())
+			count = 0
+			
+	print(count)
+	
