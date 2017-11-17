d573ab4677fa61e738adc25de51320e226333ced
Kien Ha <kienha9922@gmail.com>
2017-11-17 17:51:49 -0500

    Add working example json code
    Has working example json code that helps calls key-value pairs after
    creating Documents and placing them into the database.
    
    Signed-off-by: Kien Ha <kienha9922@gmail.com>

--- EntranceSecurity/src/Database.java
+++ EntranceSecurity/src/Database.java
@@ -1,57 +1,107 @@
+import com.mongodb.client.FindIterable;
+import static com.mongodb.client.model.Filters.*;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase; 
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 import com.mongodb.MongoClient; 
 import com.mongodb.MongoCredential;
+import com.mongodb.BasicDBObject;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.bson.Document;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Database { 
+	public static String outputFilename;
+	static List<String> list = new ArrayList<String>();
    
-   public static void main( String args[] ) {  
+   public static void main( String args[] ) { 
+	   BufferedWriter writer;
       
       // Creating a Mongo client 
       MongoClient mongo = new MongoClient( "localhost" , 27017 ); 
-   
-      // Creating Credentials 
-      MongoCredential credential; 
-      credential = MongoCredential.createCredential("sampleUser", "myDb", 
-         "password".toCharArray()); 
-      System.out.println("Connected to the database successfully");  
       
       // Accessing the database 
-      MongoDatabase database = mongo.getDatabase("myDb"); 
-      System.out.println("Credentials ::"+ credential); 
+      MongoDatabase historyDatabase = mongo.getDatabase("history");
+      MongoCollection<Document> collect = historyDatabase.getCollection("Date");
       
-      //Create collection
-      database.createCollection("sampleCollection");
-      database.getCollection("sampleCollection");
-      database.createCollection("sampleCollection2");
-      database.getCollection("sampleCollection2");
-      System.out.println("Collection created successfully");
+      Document doc = new Document("title", "hello")
+      .append("id", 1);
+      Document doc2 = new Document("hello", "world")
+    		  .append("age", 25);
+      System.out.println(doc);
+      collect.insertOne(doc);
+      collect.insertOne(doc2);
+      FindIterable<Document> iterDoc = collect.find();
+      Iterator it = iterDoc.iterator();
+      //JSONObject jobject = new JSONObject();
+      //jobject = (JSONObject) it;
+      while (it.hasNext()) {
+    	  System.out.println(it.next());
+      }
+      //System.out.println(jobject);
+      // json("hello")
       
-      for (String name: database.listCollectionNames()) {
-    	  System.out.println("Collection: " + name);
+      MongoIterable<String> mg = historyDatabase.listCollectionNames();
+      MongoCursor<String> it1 = mg.iterator();
+      
+      //https://stackoverflow.com/questions/39923406/bson-document-to-json-in-java
+      while (it1.hasNext()) {
+    	  MongoCollection<Document> table = historyDatabase.getCollection(it1.next());
+    	  
+    	  for (Document doc3 : table.find()) {
+    		  System.out.println(doc3.toJson());
+    		  String js = doc3.toJson();
+    		  //System.out.println(js);
+    		  JsonParser parser = new JsonParser();
+    		  keyValue("title", parser.parse(js));
+    		  System.out.println(list.get(0));
+    		  
+    	  }
       }
       
-      JSONObject json = new JSONObject();
-      try {
-		json.put("password", 1234);
-	} catch (JSONException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-      try {
-		String message = json.get("password").toString();
-		System.out.println(message);
-	} catch (JSONException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-      
-      database.drop();
+      historyDatabase.drop();
             
       mongo.close();
       System.out.println("Closed MongoDB");
    } 
+   
+   private static void keyValue(String key, JsonElement jsonElement) {
+
+       if (jsonElement.isJsonArray()) {
+           for (JsonElement element : jsonElement.getAsJsonArray()) {
+               keyValue(key, element);
+           }
+       } else {
+           if (jsonElement.isJsonObject()) {
+               Set<Map.Entry<String, JsonElement>> entrySet = jsonElement
+                       .getAsJsonObject().entrySet();
+               for (Map.Entry<String, JsonElement> entry : entrySet) {
+                   String tempKey = entry.getKey();
+                   if (tempKey.equals(key)) {
+                       list.add(entry.getValue().toString());
+                   }
+                   keyValue(key, entry.getValue());
+               }
+           } else {
+               if (jsonElement.toString().equals(key)) {
+                   list.add(jsonElement.toString());
+               }
+           }
+       }
+   }
 }
