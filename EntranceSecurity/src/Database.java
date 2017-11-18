007dc4e27e34850520decf277b756c26047cf15a
Kien Ha <kienha9922@gmail.com>
2017-11-17 23:58:46 -0500

    Add PIN collection
    PIN collection stores valid pin number
    
    Signed-off-by: Kien Ha <kienha9922@gmail.com>

--- EntranceSecurity/src/Database.java
+++ EntranceSecurity/src/Database.java
@@ -11,25 +11,37 @@ public class Database {
 	public static void main( String args[] ) { 
 		MongoClient mongoClient = new MongoClient();
 		MongoDatabase db = mongoClient.getDatabase("security");
-		MongoCollection<Document> collect = db.getCollection("history");
+		MongoCollection<Document> historyCollect = db.getCollection("history");
 		MongoIterable<String> strings = mongoClient.listDatabaseNames();
 		MongoCursor<String> iterator = strings.iterator();
 		Document doc = new Document("Date", "Monday")
 				.append("name", "kien");
-		collect.insertOne(doc);
+		historyCollect.insertOne(doc);
 		
 		Date date = new Date();
 		SimpleDateFormat ft = new SimpleDateFormat("yyyy.MM.dd HH:mm");
 		//System.out.println(ft.format(date));
 		Document doc2 = new Document("Date", ft.format(date));
-		collect.insertOne(doc2);
+		historyCollect.insertOne(doc2);
 		
-		FindIterable<Document> doc3 = collect.find();
+		FindIterable<Document> doc3 = historyCollect.find();
 		for (Document doc4 : doc3) {
 			if (doc4.get("Date") != null) {
 				System.out.println(doc4.get("Date"));
 			}
 		}
+		
+		MongoCollection<Document> pinCollect = db.getCollection("pin");
+		Document validPin = new Document("pin", 1234);
+		pinCollect.insertOne(validPin);
+		
+		FindIterable<Document> doc5 = pinCollect.find();
+		for (Document doc4 : doc5) {
+			if (doc4.get("pin") != null) {
+				System.out.println(doc4.get("pin"));
+			}
+		}
+		
 		db.drop();
 		mongoClient.close();
 	}
