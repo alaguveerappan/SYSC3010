25864845fe6e06c41e7e09855a0288edff6dc5cd
Carmen Ho <carmenho@cmail.carleton.ca>
2017-11-18 09:59:31 +0000

    Move connect socket inside the while so the function can be repeatable

--- Arduino/testArduinoSend5.py
+++ Arduino/testArduinoSend5.py
@@ -7,9 +7,6 @@ ser = serial.Serial("/dev/ttyACM0", 9600)
 pin = 1234
 count = 0
 
-s = socket.socket()
-s.connect(('192.168.43.37', 5054))
-
 while True:
     read_ser =  int(ser.readline())
     print(read_ser)
@@ -25,7 +22,8 @@ while True:
         if(count >= 1):
             ser.write('5'.encode())
             count = 0
-            
+            s = socket.socket()
+            s.connect(('192.168.43.37', 5055))
             s.send(b'1')
             os.chdir("/home/pi/Desktop/tcp")
             os.system("java Server localhost")
