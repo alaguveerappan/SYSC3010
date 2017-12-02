183f77ab169239e4b38aa023a4ccf8f44e8db834
Kien Ha <kienha9922@gmail.com>
2017-12-02 02:15:29 -0500

    Add checkUsersDatabase
    Check what users exists in the database
    
    Signed-off-by: Kien Ha <kienha9922@gmail.com>

---
+++ EntranceSecurity/src/checkUsersDatabase.java
@@ -0,0 +1,28 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.*;
+
+import org.bson.Document;
+
+public class checkUsersDatabase {
+
+    public static void main( String args[] ) {
+
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase db = mongoClient.getDatabase("security");
+
+        MongoCollection<Document> pinCollect = db.getCollection("pin");
+
+		FindIterable<Document> pinIterator = pinCollect.find();
+		for (Document pinDocument : pinIterator) {
+			if (pinDocument.get("pin") != null) {
+				System.out.println(pinDocument.get("pin"));
+			}
+            if (pinDocument.get("name") != null) {
+				System.out.println(pinDocument.get("name"));
+			}
+		}
+
+		mongoClient.close();
+
+    }
+}
